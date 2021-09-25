0f19e39256311138230092caee11e3a65ac15d7d
Piotr Ciszek <piotr_ciszek@poczta.onet.pl>
2021-09-26 00:49:57 +0200

    linking details from all quotes page, add if for no quotes

--- src/components/pages/NewQuote.js
+++ src/components/pages/NewQuote.js
@@ -1,7 +1,11 @@
 import QuoteForm from '../quotes/QuoteForm';
 
 const NewQuote = () => {
-    return <QuoteForm />
+const addQuoteHandler = quoteData => {
+    console.log(quoteData);
+}
+
+    return <QuoteForm onAddQuote={addQuoteHandler}/>
 };
 
 export default NewQuote;
