1f7331b58c45ccb9a24fa3898b6ffac6cdaa5397
Piotr Ciszek <piotr_ciszek@poczta.onet.pl>
2021-09-25 23:29:07 +0200

    adding 3 pages and redirection+ useParams for details

---
+++ src/components/pages/QuoteDetail.js
@@ -0,0 +1,14 @@
+import { useParams } from "react-router-dom";
+
+const QuoteDetail = () => {
+  const params = useParams();
+
+  return (
+    <section>
+      <h1>Quote Detail</h1>
+      <p>{params.quoteId}</p>
+    </section>
+  );
+};
+
+export default QuoteDetail;
